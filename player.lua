83fbcb1f9ae1abf3da6c4c9fb900824fe04d9d8f
isacben <ibsandoval@gmail.com>
2024-01-01 18:58:40 -0800

    remove diagonal movement

--- player.lua
+++ player.lua
@@ -1,8 +1,13 @@
 function p_move()
     anit=idle
 
+    local buttons_count=0 --don't allow diagonal movement
     for i=0,3 do
-        if btn(i) then
+        if btnp(i) then
+            buttons_count+=1
+            
+            if buttons_count>1 then return end
+
             px+=dirx[i+1] --dirx={-1,1,0,0}
             py+=diry[i+1] --diry={0,0,-1,1}
             pox=-dirx[i+1]*16
